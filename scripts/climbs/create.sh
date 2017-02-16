2c060fe0c7112b25d33e6b9dd24545f78ac0d50e
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-02-16 13:43:58 -0500

    Create create.sh Create is being tested

---
+++ scripts/climbs/create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/climbs"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "climb": {
+      "date": "'"${DATE}"'",
+      "grade": "'"${GRADE}"'",
+      }
+    }' # \
+  # --header "Authorization: Token token=$TOKEN"
+
+echo
